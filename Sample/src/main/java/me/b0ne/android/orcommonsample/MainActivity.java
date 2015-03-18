5fbe18d9df09306d2896bdd494f1bd11ad235bea
b0ne <ng.b0ner@gmail.com>
2015-03-18 13:05:11 +0900

    Edit ORCommon/build.gradle and test build makeJar

--- Sample/src/main/java/me/b0ne/android/orcommonsample/MainActivity.java
+++ Sample/src/main/java/me/b0ne/android/orcommonsample/MainActivity.java
@@ -1,9 +1,12 @@
 package me.b0ne.android.orcommonsample;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import me.b0ne.android.orcommon.Utils;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String name1 = "This is string name1.";
+        Log.v("TEST",name1);
+        Log.v("TEST", Utils.md5String(name1));
     }
 
     @Override
