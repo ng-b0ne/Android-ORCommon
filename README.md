727ba31a810664214ad8a24e7b16fb12ba0eff55
b0ne <ng.b0ner@gmail.com>
2015-03-20 17:10:38 +0900

    Created list view for sample and edit README file

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# ORCommon - A common library for Android App
+# ORCommon
+A common library in some cases useful behavior for Android App.
+
+# What can?
+...
+
+# Build .jar file
+Build .jar file by gradle. You can see setting in [build.gradle of library](libraries/ORCommon/build.gradle)
+
+```sh
+$ git clone https://github.com/ng-b0ne/MaterialSample MaterialSample
+$ cd MaterialSample/libraries/ORCommon
+$ ../../gradlew makeJar
+$ ll jar-file
+```
+
+# License
+
+    Copyright 2015 bone
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
--- Sample/src/main/java/me/b0ne/android/orcommonsample/MainActivity.java
+++ Sample/src/main/java/me/b0ne/android/orcommonsample/MainActivity.java
@@ -2,23 +2,47 @@ package me.b0ne.android.orcommonsample;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import me.b0ne.android.orcommon.Utils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String name1 = "This is string name1.";
-        Log.v("TEST",name1);
-        Log.v("TEST", Utils.md5String(name1));
+//        String name1 = "This is string name1.";
+//        Log.v("TEST",name1);
+//        Log.v("TEST", Utils.md5String(name1));
+
+        mListView = (ListView)findViewById(R.id.listview);
+        String[] items = getResources().getStringArray(R.array.menu_items);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        mListView.setAdapter(itemsAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                }
+            }
+        });
+
     }
 
     @Override
